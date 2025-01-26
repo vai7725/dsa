9192f3cc2089c2aded689f537569a21297056010
Vaibhav Prajapat <vaibhav.prajapat.7725@gmail.com>
2025-01-26 14:07:10 +0530

    insertion sort done

--- 10_sorting_algos/main.cpp
+++ 10_sorting_algos/main.cpp
@@ -40,6 +40,23 @@ void selectionSort(int arr[], int n) // O(n^2)
     }
 }
 
+void insertionSort(int arr[], int n) // O(n^2)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int curr = arr[i];
+        int prev = i - 1;
+
+        while (prev >= 0 && arr[prev] < curr)
+        {
+            arr[prev + 1] = arr[prev];
+            i--;
+        }
+
+        arr[prev + 1] = curr;
+    }
+}
+
 void printArray(int arr[], int n)
 {
     for (int i = 0; i < n; i++)
@@ -63,7 +80,11 @@ int main()
     // cout << "Sorted array using bubble sort" << endl;
     // printArray(arr, n);
 
-    cout << "Sorted array using selection sort" << endl;
+    // cout << "Sorted array using selection sort" << endl;
+    // selectionSort(arr, n);
+    // printArray(arr, n);
+
+    cout << "Sorted array using insertion sort" << endl;
     selectionSort(arr, n);
     printArray(arr, n);
 
