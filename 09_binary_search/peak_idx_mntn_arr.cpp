2048b84576f68bce093a69c72ed58ecf0f651a6a
Vaibhav Prajapat <vaibhav.prajapat.7725@gmail.com>
2025-02-02 18:22:23 +0530

    peak idx in mntn arr lecture done

---
+++ 09_binary_search/peak_idx_mntn_arr.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int peakIndexInMountainArray(vector<int> &arr)
+{
+    int start = 1, end = arr.size() - 2;
+
+    while (start <= end)
+    {
+        int mid = start + (end - start) / 2;
+
+        if (arr[mid] > arr[mid + 1] && arr[mid] > arr[mid - 1])
+        {
+            return mid;
+        }
+
+        else if (arr[mid] > arr[mid - 1])
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+    }
+    return -1;
+}
